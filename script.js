776a27814ec815614af64e77507093f5477edfa9
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-03 17:21:43 +0200

    [UPD] lista

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const emailList = document.getElementById('email');
 
 for (let i = 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-        const emailItem = document.createElement('div');
+        const emailItem = document.createElement('li');
         emailItem.textContent = resp.data.response;
         emailList.appendChild(emailItem);
     });
